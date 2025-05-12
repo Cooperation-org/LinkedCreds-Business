5447456d75a12654dbf996f1afec36923cc529e9
Muhammad Hany <dev.mhany@gmail.com>
2025-05-12 15:23:30 +0300

    🌈 style: Added the 5 cards and renamed creds form to it's corresponding form name

--- app/components/FileList.tsx
+++ app/components/FileList.tsx
@@ -11,7 +11,7 @@ import {
   Tooltip
 } from '@mui/material'
 import Image from 'next/image'
-import { FileItem } from '../credentialForm/form/types/Types'
+import { FileItem } from '../skillform/form/types/Types'
 import { GlobalWorkerOptions, getDocument } from 'pdfjs-dist'
 import DeleteIcon from '@mui/icons-material/Delete'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
--- app/components/credetialTracker/Page.tsx
+++ app/components/credetialTracker/Page.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
-import {
-  Box,
-  Typography,
-  Paper,
-  Container,
-  styled,
-  Card,
-  CardContent,
-  Divider
-} from '@mui/material'
+import { Box, Typography, Paper, styled, Card, CardContent } from '@mui/material'
 import { FormData } from '../../credentialForm/form/types/Types'
 import { Logo } from '../../Assets/SVGs'
 
